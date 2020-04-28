430d762cc29e10752dd44680366813c0ef889d74
Edson Onildo <inbox.edsononildo@gmail.com>
2020-04-28 16:33:21 -0300

    Changed app name

--- linux/window_configuration.cc
+++ linux/window_configuration.cc
@@ -1,5 +1,5 @@
 #include "window_configuration.h"
 
-const char *kFlutterWindowTitle = "nosso_saldo";
+const char *kFlutterWindowTitle = "Nosso Saldo";
 const unsigned int kFlutterWindowWidth = 800;
 const unsigned int kFlutterWindowHeight = 600;
